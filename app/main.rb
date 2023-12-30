97155539d5711a526522af147fb6144988b2d6e4
Maxwell Anselm <manselm@lutron.com>
2023-12-30 15:48:28 -0500

    implement ~25% of firetop mountain logic

---
+++ app/main.rb
@@ -0,0 +1,146 @@
+require 'lib/actions'
+
+$actions = [DispelMagic,CounterSpell,MagicMirror,SummonGoblin,SummonOgre,SummonTroll,SummonGiant,SummonFireElemental,SummonIceElemental,RaiseDead,Haste,TimeStop,Protection,ResistHeat,ResistCold,Paralysis,Amnesia,Fear,Confusion,CharmMonster,CharmPerson,Disease,Poison,CureLightWounds,CureHeavyWounds,AntiSpell,Blindness,Invisibility,Permanency,DelayEffect,RemoveEnchantment,Shield,MagicMissile,CauseLightWounds,CauseHeavyWounds,LightningBolt,Fireball,FingerOfDeath,FireStorm,IceStorm,Stab,Surrender]
+
+def tick args
+  args.state.options ||= $actions.map(&:name).unshift('Nothing')
+
+  args.state.west ||= Warlock.new('West', :w, :s, :d)
+  args.state.east ||= Warlock.new('East', :up, :down, :left)
+  args.state.beings ||= [args.state.west, args.state.east]
+
+  args.state.result ||= []
+
+  # if this is a haste turn, but no one has haste, skip it
+  if args.state.haste && args.state.beings.all? { |b| !b.haste? }
+    args.state.haste = false
+  end
+
+  # advance time for all beings if this is the start of a non-haste turn
+  unless args.state.haste
+    args.state.beings.each { |b| b.tick }
+  end
+
+  # get all inputs from warlocks and see if they're ready
+  both_ready = [args.state.west, args.state.east].map do |warlock|
+    # if this is a haste turn, but this warlock doesn't have haste, skip them
+    if args.state.haste && !warlock.haste?
+      next true
+    end
+
+    # order is:
+    # 1. left hand spell
+    # 2. left hand target
+    # 3. right hand spell
+    # 4. right hand target
+    # 5. repeat
+
+    # are we on step 1, 3, or 5? choose a spell
+    if warlock.choices.empty? || warlock.ready_to_resolve || (warlock.choices.length == 1 && warlock.targets.length == 1)
+      # change selection in action menu
+      if args.inputs.keyboard.key_down.send(warlock.up)
+        warlock.index -= 1
+      end
+      if args.inputs.keyboard.key_down.send(warlock.down)
+        warlock.index += 1
+      end
+      warlock.index %= args.state.options.length
+
+      # select current action
+      if args.inputs.keyboard.key_down.send(warlock.select)
+        # if we're choosing left hand, clear in case they restarted selection
+        if warlock.targets.length != 1
+          warlock.clear_input
+        end
+        warlock.choices << warlock.index
+
+        # target is next, so reset index based on default target
+        # no default target is set to self
+        default_target =
+          if args.state.west.index == 0
+            :self
+          else
+            $actions[warlock.index - 1].default_target || :self
+          end
+
+        warlock.index = args.state.beings.index do |being|
+          (default_target == :self && being == warlock) || (default_target == :other && being.is_a?(Warlock) && being != warlock)
+        end
+      end
+    elsif warlock.targets.length < 2 # are we on step 2 or 4?
+      # change selection in being menu
+      if args.inputs.keyboard.key_down.send(warlock.up)
+        warlock.index -= 1
+      end
+      if args.inputs.keyboard.key_down.send(warlock.down)
+        warlock.index += 1
+      end
+      warlock.index %= args.state.beings.length
+
+      # select current being
+      if args.inputs.keyboard.key_down.send(warlock.select)
+        warlock.targets << args.state.beings[warlock.index]
+        warlock.index = 0
+      end
+    end
+    warlock.ready_to_resolve
+  end.all?
+
+  # resolve actions
+  if both_ready
+    actions = []
+    [args.state.west, args.state.east].each do |warlock|
+      warlock.choices.each_with_index do |i, j|
+        i -= 1 # undo Nothing being added to the front of the list
+        next unless i >= 0
+
+        action = $actions[i]
+        actions << action.new(warlock, warlock.targets[j])
+      end
+    end
+
+    args.state.actions = actions.sort
+    args.state.result = []
+
+    # all monster attacks go last
+    args.state.beings.each do |being|
+      if being.is_a?(Monster) && being.health > 0 && (!args.state.haste || being.haste?)
+        args.state.actions << being.attack
+      end
+    end
+
+    # resolve each action
+    while action = args.state.actions.shift
+      action.resolve args
+    end
+
+    # clear spell selection
+    args.state.west.choices = []
+    args.state.east.choices = []
+
+    # resolve opposite turn type next
+    args.state.haste = !args.state.haste
+  end
+
+  [[args.state.west, 0, 1, 0], [args.state.east, 1280, -1, 2]].each do |warlock, edge, shift, alignment|
+    args.outputs.labels << [edge + shift * 10, 660, "#{warlock.name}: " + warlock.choices.map { |i| args.state.options[i] }.join(', '), 6, alignment]
+
+    choices =
+      if warlock.choices.empty? || warlock.ready_to_resolve || (warlock.choices.length == 1 && warlock.targets.length == 1)
+        args.state.options
+      elsif warlock.targets.length < 2
+        args.state.beings.map(&:name)
+      end
+
+    (-5..5).each do |o|
+      i = (warlock.index + o) % choices.length
+      args.outputs.labels << [edge + shift * 10, 360 - o * 40, choices[i], 5, alignment, o == 0 ? 255 : 0, 0, 0]
+    end
+  end
+
+  # draw previous resolution
+  args.state.result.each_with_index do |r, i|
+    args.outputs.labels << [640, 700 - i * 40, r, 3, 1]
+  end
+
+end
---
+++ lib/actions.rb
@@ -0,0 +1,790 @@
+class Being
+  attr_reader :name, :health
+
+  # shielded blocks most damage sources
+  # resistance is for elements
+  attr_accessor :shielded, :resistance, :haste
+
+  def initialize
+    @shielded = false
+    @resistance = {}
+    @haste = 0
+  end
+
+  def remove_enchantments
+    # TODO
+  end
+
+  def tick
+    @shielded = false
+  end
+
+  def damage x
+    @health = [@health - x, 0].max
+  end
+
+  def heal x
+    @health = [@health + x, @max_health].min
+  end
+
+  def kill
+    @health = 0
+  end
+
+  def revive
+    prev = @health
+    @health = @max_health
+    prev
+  end
+
+  def haste?
+    @haste > 0
+  end
+end
+
+# player character
+class Warlock < Being
+  attr_reader :up, :down, :select
+  attr_accessor :index, :choices, :targets
+
+  def initialize name, up, down, select
+    super()
+    @name = name
+    @up = up
+    @down = down
+    @select = select
+
+    @max_health = 15
+    @health = @max_health
+    # TODO: replace with real spell selection system
+    @index = 0
+    # left, right
+    @choices = []
+    # left, right
+    @targets = []
+  end
+
+  def clear_input
+    @choices = []
+    @targets = []
+  end
+
+  def ready_to_resolve
+    @choices.length >= 2 && @targets.length >= 2
+  end
+end
+
+# anything that can automatically attack
+class Monster < Being
+end
+
+# goblins, trolls, etc.
+class Minion < Monster
+  def self.count
+    @i ||= 0
+    @i += 1
+  end
+
+  attr_accessor :controller, :target
+
+  def initialize strength, species
+    super()
+    @max_health = strength
+    @attack = strength
+    @health = @max_health
+    # TODO: cooler name
+    @name = "#{species} #{self.class.count}"
+  end
+
+  def attack
+    Attack.new self, @target, @attack
+  end
+end
+
+# cannot be controlled, attacks indiscriminately
+class Elemental < Monster
+  attr_reader :element
+
+  def initialize strength, element
+    super()
+    @max_health = strength
+    @attack = strength
+    @health = @max_health
+    @element = element
+    @resistance = {@element => true}
+    # TODO: cooler name
+    @name = "#{element}"
+  end
+
+  def attack
+    ElementalAttack.new self, @attack, @element
+  end
+end
+
+class Action
+  class << self
+    attr_reader :name, :default_target, :order
+  end
+
+  attr_reader :origin
+  attr_accessor :user, :target
+
+  def initialize user, target
+    @origin = user
+    @user = user
+    # only actions with a default target can be targeted
+    if self.class.default_target
+      @target = target
+    end
+  end
+
+  def name
+    self.class.name
+  end
+
+  def <=> other
+    self.class.order <=> other.class.order
+  end
+
+  def description
+    "#{@user.name}: #{name} #{@target ? @target.name : nil}"
+  end
+
+  def resolve args
+    args.state.result << "#{name} does nothing"
+  end
+end
+
+class DispelMonsters < Action
+  def initialize
+    # nothing to do
+  end
+
+  def resolve args
+    args.state.beings.reject! do |being|
+      being.is_a? Monster
+    end
+  end
+end
+
+class Spell < Action
+  class << self
+    attr_reader :type
+  end
+
+  def description
+    subject =
+      if self.class.default_target == :self && @target == @user
+        ""
+      elsif @target == @user
+        " on himself"
+      elsif @target == nil
+        ""
+      else
+        " on #{@target.name}"
+      end
+    "#{@user.name} casts #{name}#{subject}"
+  end
+end
+
+class SummonSpell < Spell
+  class << self
+    attr_reader :strength
+  end
+
+  def self.type
+    :summoning
+  end
+
+  def self.name
+    "Summon #{@name}"
+  end
+
+  def self.monster_name
+    @name
+  end
+end
+
+class Attack < Action
+  @default_target = :other
+
+  def initialize user, target, str
+    super user, target
+    @strength = str
+  end
+
+  def description
+    "#{@user.name} attacks #{@target.name} (#{@strength})"
+  end
+
+  def resolve args
+    if target.shielded
+      args.state.result << "#{description} (shielded)"
+    else
+      target.damage @strength
+      args.state.result << description
+    end
+  end
+end
+
+class ElementalAttack < Action
+  def initialize user, str, element
+    super user, nil
+    @strength = str
+    @element = element
+  end
+
+  def resolve args
+    # attack all non-resistant beings
+    args.state.beings.each do |being|
+      next if being == @user # other than itself
+      if being.resistance[@element]
+        args.state.result << "#{@user.name} attacks #{being.name} (#{@strength}) (resisted)"
+      else
+        args.state.result << "#{@user.name} attacks #{being.name} (#{@strength})"
+        being.damage @strength
+      end
+    end
+  end
+end
+
+class SummonMinion < SummonSpell
+  def resolve args
+    controller =
+      case @target
+      when Minion
+        @target.controller
+      when Elemental
+        args.state.result << "#{description} (invalid target)"
+        return
+      when Warlock
+        @target
+      end
+
+    # create a minion
+    minion = Minion.new self.class.strength, self.class.monster_name
+    minion.controller = controller
+
+    # TODO: should prompt warlock for minion target on turn it is summoned
+    if controller == args.state.west
+      minion.target = args.state.east
+    else
+      minion.target = args.state.west
+    end
+
+    args.state.beings << minion
+
+    # minions immediately attack
+    args.state.actions << minion.attack
+  end
+end
+
+class SummonElemental < SummonSpell
+  class << self
+    attr_reader :element
+  end
+
+  def resolve args
+    existing = {fire: [], ice: []}
+    existing[:fire].concat args.state.beings.select { |b| b.is_a?(Elemental) && b.element == :fire }
+    existing[:ice ].concat args.state.beings.select { |b| b.is_a?(Elemental) && b.element == :ice  }
+
+    alive = existing.transform_values { |es| es.select { |b| b.health > 0 } }
+
+    summon = {fire: [], ice: []}
+    summon[self.class.element] << self
+
+    storms = {fire: [], ice: []}
+
+    args.state.actions.each do |action|
+      if action.is_a? SummonElemental
+        summon[action.class.element] << action
+      elsif action.is_a? StormSpell
+        storms[action.class.element] << action
+      end
+    end
+
+    total = {}
+    [:fire, :ice].each do |e|
+      total[e] = alive[e].length + summon[e].length + storms[e].length
+    end
+
+    # at most one storm or elemental can exist per turn
+    # if the elements mix, everything cancels
+    if total[:fire] > 0 && total[:ice] > 0
+      # kill all elementals
+      args.state.beings.each do |b|
+        next unless b.is_a? Elemental
+        b.kill
+        # remove their attacks before they take effect
+        args.state.actions.reject! { |a| a.user == b }
+      end
+      # cancel all summons and storms
+      cancelled = [self]
+      args.state.actions.reject! do |action|
+        if action.is_a?(SummonElemental) || action.is_a?(StormSpell)
+          cancelled << action
+          true
+        end
+      end
+      # if any ice storms happened, also cancel fireballs
+      if cancelled.any? { |a| a.is_a?(IceStorm) }
+        args.state.actions.reject! do |action|
+          if action.is_a? Fireball
+            cancelled << action
+            true
+          end
+        end
+      end
+      cancelled.each do |spell|
+        args.state.result << "Cancelled: #{spell.description}"
+      end
+    else # only one element is present, so _something_ will happen
+      element = total[:fire] > 0 ? :fire : :ice
+
+      if storms[element].empty? # can summon
+        # remove all redundant summons
+        args.state.actions.reject! { |a| a.is_a? SummonElemental }
+        summon[element].each do |spell|
+          args.state.result << spell.description
+        end
+
+        # heal existing elemental or summon new one
+        if existing[element].first
+          if existing[element].first.revive == 0
+            # immediately attack if it was dead
+            args.state.actions << existing[element].first.attack
+          end
+        else
+          elemental = Elemental.new(self.class.strength, element)
+          args.state.beings << elemental
+          existing[element] << elemental
+          # new elementals immediately attack
+          args.state.actions << elemental.attack
+        end
+      else # storm takes precedence
+        # kill all elementals
+        args.state.beings.each do |b|
+          next unless b.is_a? Elemental
+          b.kill
+          # remove their attacks before they take efffect
+          args.state.actions.reject! { |a| a.user == b }
+        end
+        # cancel all summons (storms will resolve later)
+        cancelled = [self]
+        args.state.actions.reject! do |action|
+          if action.is_a?(SummonElemental)
+            cancelled << action
+            true
+          end
+        end
+        cancelled.each do |spell|
+          args.state.result << "Cancelled: #{spell.description}"
+        end
+      end
+    end
+  end
+end
+
+class DispelMagic < Spell
+  @name = 'Dispel Magic'
+  @default_target = :self
+  @order = 0
+  @type = :protection
+
+  def resolve args
+    # stop all spells (resolve all other dispel magics)
+    shielded = [@user]
+    args.state.result << description
+
+    args.state.actions.reject! do |action|
+      if action.is_a? DispelMagic
+        shielded << action.user
+        args.state.result << action.description
+      elsif action.is_a? Spell
+        args.state.result << "Cancelled: #{action.description}"
+      end
+      action.is_a? Spell
+    end
+
+    # remove all enchantments from all beings
+    args.state.beings.each do |being|
+      being.remove_enchantments
+    end
+
+    # destroy all monsters (after they attack)
+    args.state.actions << DispelMonsters.new
+
+    # shield anyone who cast Dispel Magic
+    shielded.each do |being|
+      being.shielded = true
+    end
+  end
+end
+
+class CounterSpell < Spell
+  @name = 'Counter Spell'
+  @default_target = :self
+  @order = 1
+  @type = :protection
+
+  def resolve args
+    # cancel all spells cast at the target (except Finger of Death)
+    cancelled = []
+    args.state.actions.reject! do |action|
+      if action.is_a?(Spell) && action.target == @target && !action.is_a?(FingerOfDeath)
+        cancelled << action
+        true
+      end
+    end
+
+    @target.shielded = true
+
+    args.state.result << description
+    cancelled.each do |spell|
+      args.state.result << "Cancelled: #{spell.description}"
+    end
+  end
+end
+
+class MagicMirror < Spell
+  @name = 'Magic Mirror'
+  @default_target = :self
+  @order = 2
+  @type = :protection
+
+  def resolve args
+    mirrored = {@target => true}
+
+    # resolve all mirrors
+    args.state.actions.reject! do |action|
+      if action.is_a?(MagicMirror)
+        mirrored[action.target] = true
+        true
+      end
+    end
+
+    # reflect all spells against mirrored targets
+    args.state.actions.each do |action|
+      next unless action.is_a?(Spell)
+      while mirrored[action.target] && action.target != action.origin
+        action.user, action.target = action.target, action.user
+      end
+    end
+
+    args.state.result << description
+  end
+end
+
+class SummonGoblin < SummonMinion
+  @name = 'Goblin'
+  @default_target = :self
+  @order = 3
+  @strength = 1
+end
+class SummonOgre < SummonMinion
+  @name = 'Ogre'
+  @default_target = :self
+  @order = 4
+  @strength = 2
+end
+class SummonTroll < SummonMinion
+  @name = 'Troll'
+  @default_target = :self
+  @order = 5
+  @strength = 3
+end
+class SummonGiant < SummonMinion
+  @name = 'Giant'
+  @default_target = :self
+  @order = 6
+  @strength = 4
+end
+class SummonFireElemental < SummonElemental
+  @name = 'Fire Elemental'
+  @order = 7
+  @strength = 3
+  @element = :fire
+end
+class SummonIceElemental < SummonElemental
+  @name = 'Ice Elemental'
+  @order = 8
+  @strength = 3
+  @element = :ice
+end
+
+class RaiseDead < Spell
+  @name = 'Raise Dead'
+  @default_target = :self
+  @order = 9
+  @type = :protection
+
+  def resolve args
+    # cancel Fingers of Death
+    filtered = args.state.actions.reject! do |action|
+      if action.is_a? FingerOfDeath
+        args.state.result << "Cancelled: #{action.description}"
+        true
+      end
+    end
+
+    # if cancelled, also cancel all Raise Deads (including this one)
+    if filtered
+      args.state.result << "Cancelled: #{description}"
+      args.state.actions.reject! do |action|
+        if action.is_a? RaiseDead
+          args.state.result << "Cancelled: #{action.description}"
+          true
+        end
+      end
+      return
+    end
+
+    # if the target can actually be raised
+    if @target.health <= 0
+      # resolve all Raise Deads
+      raises = [self]
+      args.state.actions.reject! do |action|
+        if action.is_a? RaiseDead
+          raises << action
+          true
+        end
+      end
+
+      users = raises.map do |r|
+        args.state.result << r.description
+        r.user
+      end.uniq
+
+      if @target.is_a?(Minion)
+        if users.length == 1
+          # TODO: should prompt warlock for minion target on turn it is raised
+          @target.controller = users[0]
+          @target.target = args.state.beings.find { |b| b.is_a?(Warlock) && b != users[0] }
+        else # minion is confused
+          @target.controller = nil
+          @target.target = nil
+        end
+      end
+
+      @target.revive
+      if @target.is_a? Monster
+        args.state.actions << @target.attack
+      end
+      return
+    end
+
+    args.state.result << "#{description} (5)"
+    @target.heal 5
+  end
+end
+
+class Haste < Spell
+  @name = 'Haste'
+  @default_target = :self
+  @order = 10
+  @type = :enchantment
+
+  def resolve args
+    args.state.result << description
+    @target.haste = 3
+  end
+end
+class TimeStop < Spell
+  @name = 'Time Stop'
+  @default_target = :self
+  @order = 11
+  @type = :enchantment
+end
+class Protection < Spell
+  @name = 'Protection'
+  @default_target = :self
+  @order = 12
+  @type = :enchantment
+end
+class ResistHeat < Spell
+  @name = 'Resist Heat'
+  @default_target = :self
+  @order = 13
+  @type = :enchantment
+end
+class ResistCold < Spell
+  @name = 'Resist Cold'
+  @default_target = :self
+  @order = 14
+  @type = :enchantment
+end
+class Paralysis < Spell
+  @name = 'Paralysis'
+  @default_target = :other
+  @order = 15
+  @type = :enchantment
+end
+class Amnesia < Spell
+  @name = 'Amnesia'
+  @default_target = :other
+  @order = 16
+  @type = :enchantment
+end
+class Fear < Spell
+  @name = 'Fear'
+  @default_target = :other
+  @order = 17
+  @type = :enchantment
+end
+class Confusion < Spell
+  @name = 'Confusion'
+  @default_target = :other
+  @order = 18
+  @type = :enchantment
+end
+class CharmMonster < Spell
+  @name = 'Charm Monster'
+  @default_target = :self
+  @order = 19
+  @type = :enchantment
+end
+class CharmPerson < Spell
+  @name = 'Charm Person'
+  @default_target = :other
+  @order = 20
+  @type = :enchantment
+end
+class Disease < Spell
+  @name = 'Disease'
+  @default_target = :other
+  @order = 21
+  @type = :enchantment
+end
+class Poison < Spell
+  @name = 'Poison'
+  @default_target = :other
+  @order = 22
+  @type = :enchantment
+end
+class CureLightWounds < Spell
+  @name = 'Cure Light Wounds'
+  @default_target = :self
+  @order = 23
+  @type = :protection
+end
+class CureHeavyWounds < Spell
+  @name = 'Cure Heavy Wounds'
+  @default_target = :self
+  @order = 24
+  @type = :protection
+end
+class AntiSpell < Spell
+  @name = 'Anti Spell'
+  @default_target = :other
+  @order = 25
+  @type = :enchantment
+end
+class Blindness < Spell
+  @name = 'Blindness'
+  @default_target = :other
+  @order = 26
+  @type = :enchantment
+end
+class Invisibility < Spell
+  @name = 'Invisibility'
+  @default_target = :self
+  @order = 27
+  @type = :enchantment
+end
+class Permanency < Spell
+  @name = 'Permanency'
+  @default_target = :self
+  @order = 28
+  @type = :enchantment
+end
+class DelayEffect < Spell
+  @name = 'Delay Effect'
+  @default_target = :self
+  @order = 29
+  @type = :enchantment
+end
+class RemoveEnchantment < Spell
+  @name = 'Remove Enchantment'
+  @default_target = :other
+  @order = 30
+  @type = :protection
+end
+class Shield < Spell
+  @name = 'Shield'
+  @default_target = :self
+  @order = 31
+  @type = :protection
+end
+class MagicMissile < Spell
+  @name = 'Magic Missile'
+  @default_target = :other
+  @order = 32
+  @type = :damaging
+end
+class CauseLightWounds < Spell
+  @name = 'Cause Light Wounds'
+  @default_target = :other
+  @order = 33
+  @type = :damaging
+
+  def resolve args
+    @target.damage 2
+    args.state.result << description
+  end
+end
+class CauseHeavyWounds < Spell
+  @name = 'Cause Heavy Wounds'
+  @default_target = :other
+  @order = 34
+  @type = :damaging
+end
+class LightningBolt < Spell
+  @name = 'Lightning Bolt'
+  @default_target = :other
+  @order = 35
+  @type = :damaging
+end
+class Fireball < Spell
+  @name = 'Fireball'
+  @default_target = :other
+  @order = 36
+  @type = :damaging
+end
+class FingerOfDeath < Spell
+  @name = 'Finger of Death'
+  @default_target = :other
+  @order = 37
+  @type = :damaging
+end
+
+class StormSpell < Spell
+  class << self
+    attr_reader :element
+  end
+end
+
+class FireStorm < StormSpell
+  @name = 'Fire Storm'
+  @order = 38
+  @type = :damaging
+  @element = :fire
+end
+class IceStorm < StormSpell
+  @name = 'Ice Storm'
+  @order = 39
+  @type = :damaging
+  @element = :ice
+end
+
+class Stab < Action
+  @name = 'stab'
+  @default_target = :other
+  @order = 40
+end
+class Surrender < Action
+  @name = 'surrender'
+  @order = 41
+end
+
+
